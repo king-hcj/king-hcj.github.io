357fdcb804b152e69238d5d8eb83bbf5e0f5923e
king-hcj <cheneyking23@gmail.com>
2020-07-11 01:25:54 +0800

    添加对代码块的复制功能

---
+++ assets/js/clipboard-use.js
@@ -0,0 +1,20 @@
+/*页面载入完成后，创建复制按钮*/
+!function (e, t, a) {
+  /* code */
+  var initCopyCode = function(){
+    var copyHtml = '';
+    copyHtml += '<button style="margin-left:calc(100% - 80px)" class="btn-copy" data-clipboard-snippet="">';
+    copyHtml += '  <i class="fa fa-globe"></i><span>复制代码</span>';
+    copyHtml += '</button>';
+    // 因为使用的是谷歌代码插件样式，每个pre标签外再嵌套一层code
+    // $("pre").wrap("<code></code>");
+    // $("code pre").before(copyHtml);
+    $(".highlight pre").before(copyHtml);
+    new ClipboardJS('.btn-copy', {
+        target: function(trigger) {
+            return trigger.nextElementSibling;
+        }
+    });
+  }
+  initCopyCode();
+}(window, document);
