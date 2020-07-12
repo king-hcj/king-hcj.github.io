84b0fb174e5d10ba59148f8ccda00c5402ef70e7
king-hcj <cheneyking23@gmail.com>
2020-07-12 21:49:27 +0800

    modify

--- assets/js/clipboard-use.js
+++ assets/js/clipboard-use.js
@@ -1,7 +1,7 @@
 /*页面载入完成后，创建复制按钮*/
 !function (e, t, a) {
   /* code */
-  var initCopyCode = function(){
+  var initCopyCode = function () {
     var copyHtml = '';
     copyHtml += '<button style="margin-left:calc(100% - 80px)" class="btn-copy" data-clipboard-snippet="">';
     copyHtml += '  <i class="fa fa-globe"></i><span>复制代码</span>';
@@ -11,9 +11,9 @@
     // $("code pre").before(copyHtml);
     $(".highlight pre").before(copyHtml);
     new ClipboardJS('.btn-copy', {
-        target: function(trigger) {
-            return trigger.nextElementSibling;
-        }
+      target: function (trigger) {
+        return trigger.nextElementSibling;
+      }
     });
   }
   initCopyCode();
