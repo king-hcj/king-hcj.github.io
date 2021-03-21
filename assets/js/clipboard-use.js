59bc766f9f2e86ec5923f349a3fc5034e0968ff2
king-hcj <cheneyking23@gmail.com>
2021-03-21 21:25:29 +0800

    modify

--- assets/js/clipboard-use.js
+++ assets/js/clipboard-use.js
@@ -3,7 +3,7 @@
   /* code */
   var initCopyCode = function () {
     var copyHtml = '';
-    copyHtml += '<button style="margin-left:calc(100% - 80px)" class="btn-copy" data-clipboard-snippet="">';
+    copyHtml += '<button style="margin-left:calc(100% - 82px)" class="btn-copy" data-clipboard-snippet="">';
     copyHtml += '  <i class="fa fa-globe"></i><span>复制代码</span>';
     copyHtml += '</button>';
     // 因为使用的是谷歌代码插件样式，每个pre标签外再嵌套一层code
